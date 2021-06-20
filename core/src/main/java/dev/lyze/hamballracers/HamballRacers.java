af24fdc718ab6d0303a4276aaea06ebaa8176d3c
Lyze <11274700+lyze237@users.noreply.github.com>
2021-06-20 12:57:23 +0200

    Added logging

--- core/src/main/java/dev/lyze/hamballracers/HamballRacers.java
+++ core/src/main/java/dev/lyze/hamballracers/HamballRacers.java
@@ -1,11 +1,19 @@
 package dev.lyze.hamballracers;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import dev.lyze.hamballracers.screens.SplashScreen;
+import dev.lyze.hamballracers.utils.Logger;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class HamballRacers extends Game {
+	private static final Logger<HamballRacers> logger = new Logger<>(HamballRacers.class);
+
 	@Override
 	public void create() {
-		setScreen(new FirstScreen());
+	    Gdx.app.setLogLevel(Application.LOG_DEBUG);
+	    logger.logInfo("Welcome!");
+
+		setScreen(new SplashScreen());
 	}
 }
---
+++ core/src/main/java/dev/lyze/hamballracers/screens/SplashScreen.java
@@ -0,0 +1,20 @@
+package dev.lyze.hamballracers.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+import dev.lyze.hamballracers.utils.Logger;
+
+public class SplashScreen extends ScreenAdapter {
+	private static final Logger<SplashScreen> logger = new Logger<>(SplashScreen.class);
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void render(float delta) {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+}
